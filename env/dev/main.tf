3176dcf5f4b3f66f8b7de81096cedbf3f078ca27
luisbenjaminhortalopez <lbhortaoopez@gmail.com>
2024-10-21 14:58:57 -0600

    Adding name Luis Benjam[in Horta

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -27,6 +27,7 @@ module "dev_vm" {
 // Luis Arturo Cerecedo Enriquez
 // Mauricio Javier Carapia Barcena
 //Jared Noe Landeros Castellanos 
+//Luis Benjamín Horta López
 # Doing corrections in Docker images
 /*
 module "dev_vm" {
