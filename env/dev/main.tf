8454eff647482e81c57e5051f6da36d4c5044933
GusoDark <gusog.r.o.87@gmail.com>
2024-10-21 06:12:44 -0600

    Syntax Errors corrected

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -5,22 +5,22 @@ module "dev_vm" {
   mail_user                = var.MAIL_USER
   admin_username           = "adminuser"
   domain                   = var.DOMAIN
-  resource_group           = "MN_RG_TeamVii"  # Modificado
-  nic_name                 = "MN_NIC_TeamVii"  # Modificado
+  resource_group           = "MN-RG-TeamVii"  # Modificado
+  nic_name                 = "MN-NIC-TeamVii"  # Modificado
   mail_service             = var.MAIL_SERVICE
-  security_group_name      = "MN_SG_TeamVii"   # Modificado
+  security_group_name      = "MN-SG-TeamVii"   # Modificado
   ssh_key_path             = "./keys/712monomap_server"
   port                     = var.PORT
-  server_name              = "MN_Server_TeamVii"  # Modificado
+  server_name              = "MN-Server-TeamVii"  # Modificado
   location                 = "eastus2"
   mapbox_access_token      = var.MAPBOX_ACCESS_TOKEN
   mongo_url                = var.MONGO_URL
-  subnet_name              = "MN_SUBNET_TeamVii"  # Modificado
+  subnet_name              = "MN-SUBNET-TeamVii"  # Modificado
   mongo_init_root_username = var.MONGO_INITDB_ROOT_USERNAME
   mongo_init_root_password = var.MONGO_INITDB_ROOT_PASSWORD
   mongo_db                 = var.MONGO_DB
-  ip_name                  = "MN_IP_TeamVii"  # Modificado
-  vnet_name                = "MN_VNET_TeamVii"  # Modificado
+  ip_name                  = "MN-IP-TeamVii"  # Modificado
+  vnet_name                = "MN-VNET-TeamVii"  # Modificado
 }
 
 # testing again
